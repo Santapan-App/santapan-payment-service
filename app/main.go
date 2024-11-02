ed86aae8e82a5368cad8299a8f1c99a082983bbe
Jan Falih Fadhillah <54056113+jfalih@users.noreply.github.com>
2024-11-02 09:58:46 +0700

    Change ci cd env fix

--- app/main.go
+++ app/main.go
@@ -18,7 +18,8 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/source/file" // Import the file source driver
+	_ "github.com/golang-migrate/migrate/v4/database/postgres" // Import the postgres driver for migrations
+	_ "github.com/golang-migrate/migrate/v4/source/file"       // Import the file source driver
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq" // Import the PostgreSQL driver
